25af8bca08cb3605c85941a554f34c6beed78c4b
nikkeykashyap45-byte <nikkeykashyap45@gmail.com>
2026-07-13 11:47:38 +0530

    Time: 0 ms (100%), Space: 47.1 MB (82.1%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int exceptedSum = n*(n+1)/2;
+        int actualSum=0;
+        for(int num:nums){
+            actualSum += num;
+        }
+        return exceptedSum - actualSum;
+    }
+}
