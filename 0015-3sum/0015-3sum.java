e52dc5249fb62578a7d5a1e291a8aebdde13cbee
Nikky Dheevar <nikkeykashyap45@gmail.com>
2026-09-10 12:32:17 +0530

    Time: 33 ms (70.92%), Space: 59.2 MB (59.57%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -11,9 +11,7 @@ class Solution {
             while(left<right){
                 int sum = nums[i]+nums[left]+nums[right];
                 if(sum==0){
-                    result.add(Arrays.asList(
-                        nums[i],nums[left],nums[right]
-                    ));
+                    result.add(Arrays.asList(nums[i],nums[left],nums[right]));
                     while(left<right && nums[left]==nums[left+1]){
                         left++;
                     }
