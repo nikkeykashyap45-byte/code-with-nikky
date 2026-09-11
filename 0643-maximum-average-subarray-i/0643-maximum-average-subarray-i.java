884f1538d77733ac91c17cde492ba60f2544fc5d
Nikky Dheevar <nikkeykashyap45@gmail.com>
2026-09-11 11:46:35 +0530

    Time: 2 ms (99.85%), Space: 69.7 MB (25.6%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.java
@@ -1,24 +1,15 @@
 class Solution {
     public double findMaxAverage(int[] nums, int k) {
-
         int windowSum = 0;
-
-        // First window ka sum
-        for (int i = 0; i < k; i++) {
-            windowSum += nums[i];
+        for(int i=0;i<k;i++){
+            windowSum+=nums[i];
         }
-
         int maxSum = windowSum;
-
-        // Window ko slide karna
-        for (int i = k; i < nums.length; i++) {
-
-            windowSum += nums[i];       // new element add
-            windowSum -= nums[i - k];   // old element remove
-
-            maxSum = Math.max(maxSum, windowSum);
+        for(int i=k;i<nums.length;i++){
+            windowSum += nums[i];
+            windowSum -= nums[i-k];
+            maxSum = Math.max(maxSum,windowSum);
         }
-
-        return (double) maxSum / k;
+        return (double) maxSum/k;
     }
 }
